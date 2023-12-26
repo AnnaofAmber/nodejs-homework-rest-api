5b97bdb7b7a9b6522aecf9df345a662bc7828d07
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-12-26 08:38:17 +0200

    adding api

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
-const { validateBody, isValidId } = require("../../middlewares");
-const { schemas } = require("../../models/contacts");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
 
 const ctrl = require("../../controllers/contacts");
 
-router.get("/", ctrl.getAllContacts);
+router.get("/", authenticate, ctrl.getAllContacts);
 
-router.get("/:contactId", isValidId, ctrl.getContactById);
+router.get("/:contactId", authenticate, isValidId, ctrl.getContactById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.addContact
+);
 
-router.delete("/:contactId", isValidId, ctrl.deleteContact);
+router.delete("/:contactId", authenticate, isValidId, ctrl.deleteContact);
 
 router.put(
   "/:contactId",
+  authenticate,
   isValidId,
   validateBody(schemas.updateSchema),
   ctrl.updateContact
 );
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateFavorite
