3c91fe77f5e1cc7fe9f126deca1275c24ca41dc3
Anna Boichuk <anna.boichuk.o@gmail.com>
2024-01-12 18:14:46 +0200

    switching to sendgrid

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,26 +1,15 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
 
-const { META_PASSWORD } = process.env;
+const { SENDGRID_API_KEY } = process.env;
 
-const nodemailerConfig = {
-  host: "smtp.meta.ua",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "annamats98@meta.ua",
-    pass: META_PASSWORD,
-  },
-};
-
-const transporter = nodemailer.createTransport(nodemailerConfig);
+sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "annamats98@meta.ua" };
-  await transporter
-    .sendMail(email)
-    .then(() => console.log("Email sent successfully"))
-    .catch((error) => console.log(error.message));
+  const email = { ...data, from: "anyutyk.boychuk@gmail.com" };
+
+  await sgMail.send(email);
+
   return true;
 };
 
